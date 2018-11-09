e483e1f188ed5b0fe4f0ea624a2b0a50908d0733
= <=>
2018-11-09 16:42:06 +0100

    Added show functionality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,8 +7,6 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
-
-
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -36,6 +34,8 @@ class PortfoliosController < ApplicationController
     end
   end
 
-
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
 end
