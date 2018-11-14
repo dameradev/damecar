b0a15672ad839364cee7be195d958146224ebcb4
= <=>
2018-11-14 23:21:34 +0100

    Completed authentication with virtual attributes

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -4,7 +4,7 @@ module DeviseWhitelist
   included do
     before_action :configure_permited_parameters, if: :devise_controller?
   end
-  
+
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
