07348c19834a3844708c09ffb0220b497179f63e
= <=>
2018-11-20 19:27:14 +0100

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
      "<br>".html_safe + (link_to "Login", new_user_session_path)
    end
   end
+
+  def source_helper (layout_name)
+     if session[:source]
+       greeting = "Thanks for visiting me from  #{session[:source]} and you are visiting me from #{layout_name}"
+       content_tag(:p, greeting, class: "source-greeting" )
+     end
+  end
+
+  # def sample_helper
+  #   content_tag(:div, ' My content', class: 'my_class')
+  # end
 end
