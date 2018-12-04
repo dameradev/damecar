12cc3da42cea213c88c982fe6e9adebf44d8b10b
= <=>
2018-12-04 11:33:08 +0100

    Added navigation

--- config/routes.rb
+++ config/routes.rb
@@ -14,21 +14,10 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  mount ActionCable.server => '/cable'
-=======
   get 'tech-news', to: "pages#tech_news"
->>>>>>> lib
-=======
-
-  get 'tech-news', to: "pages#tech_news"
-
 
   mount ActionCable.server => '/cable'
 
->>>>>>> 1b9ca647389102acee1c0aa8af06840282ad1062
   root to: 'pages#home'
 
 
