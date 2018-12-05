0e056228fc99faf793c00c512e9d36c0d10c1cfd
= <=>
2018-12-05 20:10:21 +0100

    Implemented redirect for draft page when non admin tries to access it

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,12 +25,15 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
+    if @blog.published? || logged_in?(:site_admin)
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
 
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
-
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "Not authorized"
+    end
   end
 
   # GET /blogs/new
