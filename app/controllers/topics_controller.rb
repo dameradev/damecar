a5995914a692e9d86522ff452a43809771e85ef9
= <=>
2018-12-06 19:40:04 +0100

    Implemented blog widget

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,17 +1,12 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show]
+  before_action :set_sidebar_topics
   layout 'blog'
   def index
     @topics = Topic.all
   end
 
   def show
-  end
-
-  private
-  def set_topic
-    @topic = Topic.find(params[:id])
-
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(5)
     else
@@ -19,7 +14,19 @@ class TopicsController < ApplicationController
     end
   end
 
+  private
+  def set_topic
+    @topic = Topic.find(params[:id])
+
+
+  end
+
   def topic_params
     params.require(:topic).permit(:title)
   end
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
+  end
+
 end
